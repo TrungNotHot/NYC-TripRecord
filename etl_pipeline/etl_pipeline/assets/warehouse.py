c17cf714bc8c5c56c4ac2e0f26555a73d62c6865
Akkashij <93694804+Akkashij@users.noreply.github.com>
2023-12-19 15:52:10 +0700

    warehouse

--- etl_pipeline/etl_pipeline/assets/warehouse.py
+++ etl_pipeline/etl_pipeline/assets/warehouse.py
@@ -3,31 +3,6 @@ from dagster import asset, AssetIn, Output, StaticPartitionsDefinition
 import polars as pl
 import pyarrow as pa
 from pyspark.sql import DataFrame
-from datetime import datetime, timedelta
-
-
-def generate_weekly_dates(start_date_str, end_date_str):
-    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
-    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
-    
-    current_date = start_date
-    while current_date < end_date:
-        yield current_date.strftime("%Y-%m-%d")
-        current_date += timedelta(weeks=1)
-def generate_3days_dates(start_date_str, end_date_str):
-    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
-    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
-    
-    current_date = start_date
-    while current_date < end_date:
-        yield current_date.strftime("%Y-%m-%d")
-        current_date += timedelta(days=3)
-start_date_str = "2023-01-01"
-end_date_str = "2023-04-01"
-three_days = list(generate_3days_dates(start_date_str, end_date_str))
-weekly_dates = list(generate_weekly_dates(start_date_str, end_date_str))
-WEEKLY = StaticPartitionsDefinition(weekly_dates)
-THREE_DAYS = StaticPartitionsDefinition(three_days)
 
 
 @asset(
@@ -50,19 +25,20 @@ THREE_DAYS = StaticPartitionsDefinition(three_days)
 def warehouse_pickup(context, gold_pickup: DataFrame):
 
 
-    context.log.info("Got spark DataFrame, loading to postgres")
+    # context.log.info("Got spark DataFrame, loading to postgres")
 
-    df = pl.from_arrow(pa.Table.from_batches(gold_pickup._collect_as_arrow()))
-    context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
+    # df = pl.from_arrow(pa.Table.from_batches(gold_pickup._collect_as_arrow()))
+    # context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
 
     return Output(
-        value=df,
+        value=gold_pickup,
         metadata={
             "database": "warehouse_pickup",
             "schema": "gold",
             "table": "warehouse_pickup",
             "primary_keys": ["PickUpID"],
             "columns": ["PickUpID", "PULocationID", "pickup_datetime"],
+            "row_count": gold_pickup.count(),
         },
     )
 
@@ -87,19 +63,20 @@ def warehouse_pickup(context, gold_pickup: DataFrame):
 def warehouse_dropoff(context, gold_dropoff: DataFrame):
 
 
-    context.log.info("Got spark DataFrame, loading to postgres")
+    # context.log.info("Got spark DataFrame, loading to postgres")
 
-    df = pl.from_arrow(pa.Table.from_batches(gold_dropoff._collect_as_arrow()))
-    context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
+    # df = pl.from_arrow(pa.Table.from_batches(gold_dropoff._collect_as_arrow()))
+    # context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
 
     return Output(
-        value=df,
+        value=gold_dropoff,
         metadata={
             "database": "warehouse_dropoff",
             "schema": "gold",
             "table": "warehouse_dropoff",
             "primary_keys": ["DropOffID"],
             "columns": ["DropOffID", "DOLocationID", "Dropoff_datetime"],
+            "row_count": gold_dropoff.count(),
         },
     )
 
@@ -124,19 +101,20 @@ def warehouse_dropoff(context, gold_dropoff: DataFrame):
 def warehouse_payment(context, gold_payment: DataFrame):
 
 
-    context.log.info("Got spark DataFrame, loading to postgres")
+    # context.log.info("Got spark DataFrame, loading to postgres")
 
-    df = pl.from_arrow(pa.Table.from_batches(gold_payment._collect_as_arrow()))
-    context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
+    # df = pl.from_arrow(pa.Table.from_batches(gold_payment._collect_as_arrow()))
+    # context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
 
     return Output(
-        value=df,
+        value=gold_payment,
         metadata={
             "database": "warehouse_payment",
             "schema": "gold",
             "table": "warehouse_payment",
             "primary_keys": ["PaymentID"],
             "columns": ["PaymentID", "fare_amount", "mta_tax", "improvement_surcharge", "payment_type", "RatecodeID", "extra", "tip_amount", "tolls_amount", "total_amount", "congestion_surcharge", "airport_fee"],
+            "row_count": gold_payment.count(),
         },
     )
 
@@ -163,19 +141,20 @@ def warehouse_payment(context, gold_payment: DataFrame):
 def warehouse_tripinfo(context, gold_info: DataFrame):
 
 
-    context.log.info("Got spark DataFrame, loading to postgres")
+    # context.log.info("Got spark DataFrame, loading to postgres")
 
-    df = pl.from_arrow(pa.Table.from_batches(gold_info._collect_as_arrow()))
-    context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
+    # df = pl.from_arrow(pa.Table.from_batches(gold_info._collect_as_arrow()))
+    # context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
 
     return Output(
-        value=df,
+        value=gold_info,
         metadata={
             "database": "warehouse_tripinfo",
             "schema": "gold",
             "table": "warehouse_tripinfo",
             "primary_keys": ["PickUpID", "DropOffID", "PaymentID"],
             "columns": ["PickUpID", "DropOffID", "PaymentID", "VendorID", "passenger_count", "trip_distance", "store_and_fwd_flag"],
+            "row_count": gold_info.count(),
         },
     )
 
@@ -202,19 +181,20 @@ def warehouse_tripinfo(context, gold_info: DataFrame):
 def warehouse_fhvinfo(context, gold_fhv_info: DataFrame):
 
 
-    context.log.info("Got spark DataFrame, loading to postgres")
+    # context.log.info("Got spark DataFrame, loading to postgres")
 
-    df = pl.from_arrow(pa.Table.from_batches(gold_fhv_info._collect_as_arrow()))
-    context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
+    # df = pl.from_arrow(pa.Table.from_batches(gold_fhv_info._collect_as_arrow()))
+    # context.log.debug(f"Got polars DataFrame with shape: {df.shape}")
 
     return Output(
-        value=df,
+        value=gold_fhv_info,
         metadata={
             "database": "warehouse_fhvinfo",
             "schema": "gold",
             "table": "warehouse_fhvinfo",
             "primary_keys": ["PickUpID", "DropOffID"],
             "columns": ["PickUpID", "DropOffID", "dispatching_base_num", "affiliated_base_number", "sr_flag"],
+            "row_count": gold_fhv_info.count(),
         },
     )
 
